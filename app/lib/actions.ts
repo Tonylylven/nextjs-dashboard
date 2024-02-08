0ed33e1fd37de6ccd5ebd3e8fb411400266a1bbd
Forsek-beep <romanionin075@gmail.com>
2024-02-08 17:03:13 +0300

    upd

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod'
 import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 const FormSchema = z.object({
     id: z.string(),
@@ -12,14 +14,60 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true})
+const UpdateInvoice = FormSchema.omit({ id: true, date: true})
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData.entries()))
     const amountInCents = amount * 100
     const date = new Date().toISOString().split('T')[0]
+    console.log(date)
 
+    try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `
+    } catch (error) {
+        return{
+            message: 'Database Error: Failed to Create Invoice.'
+        }
+    }
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse(Object.fromEntries(formData.entries()))
+
+    const amountInCents = amount * 100
+
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+        `
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Update Invoice.'
+        }
+    }
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+    throw new Error('Not implemented yet.')
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`
+        revalidatePath('/dashboard/invoices')
+        return {
+            message: 'Invoice Deleted.'
+        }
+    } catch (error) {
+        return {
+            message: 'Database error: Failed to Delete Invoice.'
+        }
+    }
 }
